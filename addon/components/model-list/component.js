653e0e96207493e3c498a278feff18815f9928cf
Ilya Radchenko <ilya@burstcreations.com>
2016-11-16 21:57:52 -0500

    USe service for admin route name

--- addon/components/model-list/component.js
+++ addon/components/model-list/component.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import layout from './template';
 
+const { inject } = Ember;
+
 export default Ember.Component.extend({
-  layout
+  layout,
+  adminConfig: inject.service()
 });
